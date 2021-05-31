90b32878b34f70b49fe63f1c19c2118f0b13e484
zachary7829 <56700161+zachary7829@users.noreply.github.com>
2021-05-31 03:58:23 -0400

    Fixed bug, will fix more ltr
    I have no idea why but results for updated and results for rollback don't work ;3;

--- main.js
+++ main.js
@@ -24,12 +24,12 @@ apioutput = JSON.parse(xhr.responseText);
 
 if (inputdict.CurrentVersion == apioutput.Version)
 {
-  result = '{"result":"updated","NewVersion":"' + apioutput.Version + '","QuickUpdate Version":"3.4","OldVersion":"' + inputdict.CurrentVersion + '","UCFU":{"Shortcut Name":"' + inputdict.ShortcutName + '","Current Version":"' + inputdict.CurrentVersion + '"RoutineHub ID":"' + inputdict.ID + '"}}';
+  result = '{"result":"updated","NewVersion":"' + apioutput.Version + '","QuickUpdate Version":"3.4","OldVersion":"' + inputdict.CurrentVersion + '","UCFU":{"Shortcut Name":"' + inputdict.ShortcutName + '","Current Version":"' + inputdict.CurrentVersion + '","RoutineHub ID":"' + inputdict.ID + '"}}';
 } else {
   if (inputdict.CurrentVersion > apioutput.Version) {
-    result = '{"result":"rollback","NewVersion":"' + NewVersion + '","QuickUpdate Version":"3.4","OldVersion":"' + inputdict.CurrentVersion + '","UCFU":{"Shortcut Name":"' + inputdict.ShortcutName + '","Current Version":"' + inputdict.CurrentVersion + '"RoutineHub ID":"' + inputdict.ID + '"}}';
+    result = '{"result":"rollback","NewVersion":"' + NewVersion + '","QuickUpdate Version":"3.4","OldVersion":"' + inputdict.CurrentVersion + '","UCFU":{"Shortcut Name":"' + inputdict.ShortcutName + '","Current Version":"' + inputdict.CurrentVersion + '","RoutineHub ID":"' + inputdict.ID + '"}}';
   } else {
-    result = '{"result":"update","NewVersion":"' + apioutput.Version + '","QuickUpdate Version":"3.4","OldVersion":"' + inputdict.CurrentVersion + '","UCFU":{"Shortcut Name":"' + inputdict.ShortcutName + '","Current Version":"' + inputdict.CurrentVersion + '"RoutineHub ID":"' + inputdict.ID + '"}}';
+    result = '{"result":"update","NewVersion":"' + apioutput.Version + '","QuickUpdate Version":"3.4","OldVersion":"' + inputdict.CurrentVersion + '","UCFU":{"Shortcut Name":"' + inputdict.ShortcutName + '","Current Version":"' + inputdict.CurrentVersion + '","RoutineHub ID":"' + inputdict.ID + '"}}';
   }
 }
 
