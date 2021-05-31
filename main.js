28481bf646838be62ba3134e51555890a9381ebe
zachary7829 <56700161+zachary7829@users.noreply.github.com>
2021-05-31 03:20:36 -0400

    Output OldVersion + UCFU Dic + Use of ShortcutName

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-inputdict = {"CurrentVersion":"1.0","ID":"26470","Service":"RoutineHub"};
+inputdict = {"CurrentVersion":"1.0","ID":"26470","Service":"RoutineHub","ShortcutName":"QuickUpdate"};
 
 if (inputdict.ID == 0)
 {
-  inputdict = {"CurrentVersion":"3.4","ID":"2647","Service":"RoutineHub"}
+  inputdict = {"CurrentVersion":"3.4","ID":"2647","Service":"RoutineHub","ShortcutName":"QuickUpdate"}
 } else {
   inputdict.ID = inputdict.ID / 10
 }
@@ -24,12 +24,12 @@ apioutput = JSON.parse(xhr.responseText);
 
 if (inputdict.CurrentVersion == apioutput.Version)
 {
-  result = '{"result":"updated","NewVersion":"' + apioutput.Version + '","QuickUpdate Version":"3.4"}';
+  result = '{"result":"updated","NewVersion":"' + apioutput.Version + '","QuickUpdate Version":"3.4","OldVersion":"' + inputdict.CurrentVersion + '","UCFU":{"Shortcut Name":"' + inputdict.ShortcutName + '","Current Version":"' + inputdict.CurrentVersion + '"RoutineHub ID":"' + inputdict.ID + '"}}';
 } else {
   if (inputdict.CurrentVersion > apioutput.Version) {
-    result = '{"result":"rollback","NewVersion":"' + NewVersion + '","QuickUpdate Version":"3.4"}';
+    result = '{"result":"rollback","NewVersion":"' + NewVersion + '","QuickUpdate Version":"3.4","OldVersion":"' + inputdict.CurrentVersion + '","UCFU":{"Shortcut Name":"' + inputdict.ShortcutName + '","Current Version":"' + inputdict.CurrentVersion + '"RoutineHub ID":"' + inputdict.ID + '"}}';
   } else {
-    result = '{"result":"update","NewVersion":"' + apioutput.Version + '","QuickUpdate Version":"3.4"}';
+    result = '{"result":"update","NewVersion":"' + apioutput.Version + '","QuickUpdate Version":"3.4","OldVersion":"' + inputdict.CurrentVersion + '","UCFU":{"Shortcut Name":"' + inputdict.ShortcutName + '","Current Version":"' + inputdict.CurrentVersion + '"RoutineHub ID":"' + inputdict.ID + '"}}';
   }
 }
 
