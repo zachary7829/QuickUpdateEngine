fd2de8a97eab9503959d9fbb998e29081ec606a7
zachary7829 <56700161+zachary7829@users.noreply.github.com>
2021-05-31 14:24:28 -0400

    Fix Rollback Output issue
    Fixed issue with rollbacks and now declares inputdict as variable (thanks mvan231!)

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-inputdict = {"CurrentVersion":"1.0","ID":"26470","Service":"RoutineHub","ShortcutName":"QuickUpdate"};
+let inputdict = {"CurrentVersion":"1.0","ID":"26470","Service":"RoutineHub","ShortcutName":"QuickUpdate"};
 
 if (inputdict.ID == 0)
 {
-  inputdict = {"CurrentVersion":"3.4","ID":"2647","Service":"RoutineHub","ShortcutName":"QuickUpdate"}
+  inputdict = {"CurrentVersion":"3.4","ID":"2647","Service":"RoutineHub","ShortcutName":"QuickUpdate"};
 } else {
-  inputdict.ID = inputdict.ID / 10
+  inputdict.ID = inputdict.ID / 10;
 }
 
 if (inputdict.Service.includes(':'))
@@ -12,8 +12,8 @@ if (inputdict.Service.includes(':'))
   inputdict.Service = 'RoutineHub';
 }
 
-document.write('{"result":"NO INTERNET/ROUTINEHUB DOWN"}')
-let xhr = new XMLHttpRequest()
+document.write('{"result":"NO INTERNET/ROUTINEHUB DOWN"}');
+let xhr = new XMLHttpRequest();
 if (inputdict.Service == 'RoutineHub') {
  xhr.open('GET','https://routinehub.co/api/v1/shortcuts/' + inputdict.ID + '/versions/latest',false);
 } else {
@@ -25,18 +25,16 @@ apioutput = JSON.parse(xhr.responseText);
 if (inputdict.CurrentVersion == apioutput.Version)
 {
   result = '{"result":"updated","NewVersion":"' + apioutput.Version + '","QuickUpdate Version":"3.4","OldVersion":"' + inputdict.CurrentVersion + '","UCFU":{"Shortcut Name":"' + inputdict.ShortcutName + '","Current Version":"' + inputdict.CurrentVersion + '","RoutineHub ID":"' + inputdict.ID + '"}}';
-} else {
+} else
+{
   if (inputdict.CurrentVersion > apioutput.Version) {
-    result = '{"result":"rollback","NewVersion":"' + NewVersion + '","QuickUpdate Version":"3.4","OldVersion":"' + inputdict.CurrentVersion + '","UCFU":{"Shortcut Name":"' + inputdict.ShortcutName + '","Current Version":"' + inputdict.CurrentVersion + '","RoutineHub ID":"' + inputdict.ID + '"}}';
+    result = '{"result":"rollback","NewVersion":"' + inputdict.NewVersion + '","QuickUpdate Version":"3.4","OldVersion":"' + inputdict.CurrentVersion + '","UCFU":{"Shortcut Name":"' + inputdict.ShortcutName + '","Current Version":"' + inputdict.CurrentVersion + '","RoutineHub ID":"' + inputdict.ID + '"}}';
   } else {
     result = '{"result":"update","NewVersion":"' + apioutput.Version + '","QuickUpdate Version":"3.4","OldVersion":"' + inputdict.CurrentVersion + '","UCFU":{"Shortcut Name":"' + inputdict.ShortcutName + '","Current Version":"' + inputdict.CurrentVersion + '","RoutineHub ID":"' + inputdict.ID + '"}}';
   }
 }
 
-if (result.includes('a'))
-{} else {
-  result = '{"result":"NO INTERNET/ROUTINEHUB DOWN"}'
-}
+if (!(result.includes('a')))result = '{"result":"NO INTERNET/ROUTINEHUB DOWN"}';
 
 document.body.innerHTML = '';
 document.write(result);
