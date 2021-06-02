f40c5e4b339b6afeae8d3fccd1489e8a55d14ed2
zachary7829 <56700161+zachary7829@users.noreply.github.com>
2021-06-02 00:20:53 -0400

    Haven't tested but hopefully works
    Fixed a bug with offline & custom servers interacting with UCFU and *should* no longer require the ID key to be 10 if using a custom server.

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 let inputdict = {"CurrentVersion":"1.0","ID":"26470","Service":"RoutineHub","ShortcutName":"QuickUpdate"};
 
-if (inputdict.ID == 0)
-{
-  inputdict = {"CurrentVersion":"3.4","ID":"2647","Service":"RoutineHub","ShortcutName":"QuickUpdate"};
-} else {
-  inputdict.ID = inputdict.ID / 10;
-}
-
 if (!(inputdict.Service.includes(':'))) {
   inputdict.Service = 'RoutineHub';
 }
 
-document.write('{"result":"NO INTERNET/ROUTINEHUB DOWN"}');
+if (inputdict.Service == 'RoutineHub') {
+  if (inputdict.ID == 0) {
+    inputdict = {"CurrentVersion":"3.4","ID":"2647","Service":"RoutineHub","ShortcutName":"QuickUpdate"};
+    document.write('{"result":"NO INTERNET/ROUTINEHUB DOWN","UCFU":{"Shortcut Name":"' + inputdict.ShortcutName + '","Current Version":"' + inputdict.CurrentVersion + '","RoutineHub ID":"' + inputdict.ID + '"}}');
+  } else {
+    inputdict.ID = inputdict.ID / 10;
+    document.write('{"result":"NO INTERNET/ROUTINEHUB DOWN","UCFU":{"Shortcut Name":"' + inputdict.ShortcutName + '","Current Version":"' + inputdict.CurrentVersion + '","RoutineHub ID":"' + inputdict.ID + '"}}');
+  }
+} else {
+  //If using a custom server QUEngine returns QU's dict for UCFU to not mess up UltraUpdate
+  document.write('{"result":"NO INTERNET/ROUTINEHUB DOWN","UCFU":{"CurrentVersion":"3.4","ID":"2647","Service":"RoutineHub","ShortcutName":"QuickUpdate"}}');
+}
+
 let xhr = new XMLHttpRequest();
 if (inputdict.Service == 'RoutineHub') {
  xhr.open('GET','https://routinehub.co/api/v1/shortcuts/' + inputdict.ID + '/versions/latest',false);
