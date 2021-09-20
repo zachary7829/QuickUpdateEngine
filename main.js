72a0202790fb5f76cb32637eb8dcf2c7489ef6b6
zachary7829 <56700161+zachary7829@users.noreply.github.com>
2021-09-19 22:03:50 -0400

    Build 2021-09-19
    Not the complete build, but almost there!
    
    Changed in this build:
    -Changed 3.4 to 4.0
    -Slightly better error handling, although still could use some work
    
    To do:
    -Some tidying up with Custom Servers and how they're handled
    -Better error handling
    -Still gotta work on not constructing json from strings.

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//QuickUpdateEngineAPI Build 2021-09-19, Created by Zachary Keffaber (zachary7829), open sourced under GPLv3 License.
+
 let inputdict = {"CurrentVersion":"1.0","ID":"2647","Service":"RoutineHub","ShortcutName":"QuickUpdate"};
 
 if (!(inputdict.Service.includes(':'))) {
@@ -6,14 +8,14 @@ if (!(inputdict.Service.includes(':'))) {
 
 if (inputdict.Service == 'RoutineHub') {
   if(!(parseFloat(inputdict.ID)==inputdict.ID)) {
-    inputdict = {"CurrentVersion":"3.4","ID":2647,"Service":"RoutineHub","ShortcutName":"QuickUpdate"};
-    document.write('{"result":"NO INTERNET/ROUTINEHUB DOWN","UCFU":{"Shortcut Name":"' + inputdict.ShortcutName + '","Current Version":"' + inputdict.CurrentVersion + '","RoutineHub ID":"' + inputdict.ID + '"}}');
+    inputdict = {"CurrentVersion":"4.0","ID":2647,"Service":"RoutineHub","ShortcutName":"QuickUpdate"};
+    document.write('{"result":"Updated","errors":"NO INTERNET/ROUTINEHUB DOWN","UCFU":{"Shortcut Name":"' + inputdict.ShortcutName + '","Current Version":"' + inputdict.CurrentVersion + '","RoutineHub ID":"' + inputdict.ID + '"}}');
   } else {
-    document.write('{"result":"NO INTERNET/ROUTINEHUB DOWN","UCFU":{"Shortcut Name":"' + inputdict.ShortcutName + '","Current Version":"' + inputdict.CurrentVersion + '","RoutineHub ID":"' + inputdict.ID + '"}}');
+    document.write('{"result":"Updated","errors":"NO INTERNET/ROUTINEHUB DOWN","UCFU":{"Shortcut Name":"' + inputdict.ShortcutName + '","Current Version":"' + inputdict.CurrentVersion + '","RoutineHub ID":"' + inputdict.ID + '"}}');
   }
 } else {
-  //If using a custom server QUEngine returns QU's dict for UCFU to not mess up UltraUpdate
-  document.write('{"result":"NO INTERNET/ROUTINEHUB DOWN","UCFU":{"Current Version":"3.4","RoutineHub ID":"2647","Service":"RoutineHub","Shortcut Name":"QuickUpdate"}}');
+  //If using a custom server QUEngine returns QU's dict for UCFU to not fuck up UltraUpdate
+  document.write('{"result":"Updated","errors":"NO INTERNET/ROUTINEHUB DOWN","UCFU":{"Current Version":"4.0","RoutineHub ID":"2647","Service":"RoutineHub","Shortcut Name":"QuickUpdate"}}');
 }
 
 let xhr = new XMLHttpRequest();
@@ -27,17 +29,17 @@ apioutput = JSON.parse(xhr.responseText);
 
 if (inputdict.CurrentVersion == apioutput.Version)
 {
-  result = '{"result":"Updated","NewVersion":"' + apioutput.Version + '","QuickUpdate Version":"3.4","OldVersion":"' + inputdict.CurrentVersion + '","UCFU":{"Shortcut Name":"' + inputdict.ShortcutName + '","Current Version":"' + inputdict.CurrentVersion + '","RoutineHub ID":"' + inputdict.ID + '"}}';
+  result = '{"result":"Updated","errors":"none","NewVersion":"' + apioutput.Version + '","QuickUpdate Version":"4.0","OldVersion":"' + inputdict.CurrentVersion + '","UCFU":{"Shortcut Name":"' + inputdict.ShortcutName + '","Current Version":"' + inputdict.CurrentVersion + '","RoutineHub ID":"' + inputdict.ID + '"}}';
 } else
 {
   if (inputdict.CurrentVersion > apioutput.Version) {
-    result = '{"result":"Rollback","NewVersion":"' + apioutput.Version + '","QuickUpdate Version":"3.4","OldVersion":"' + inputdict.CurrentVersion + '","UCFU":{"Shortcut Name":"' + inputdict.ShortcutName + '","Current Version":"' + inputdict.CurrentVersion + '","RoutineHub ID":"' + inputdict.ID + '"}}';
+    result = '{"result":"Rollback","errors":"none","NewVersion":"' + apioutput.Version + '","QuickUpdate Version":"4.0","OldVersion":"' + inputdict.CurrentVersion + '","UCFU":{"Shortcut Name":"' + inputdict.ShortcutName + '","Current Version":"' + inputdict.CurrentVersion + '","RoutineHub ID":"' + inputdict.ID + '"}}';
   } else {
-    result = '{"result":"Update","NewVersion":"' + apioutput.Version + '","QuickUpdate Version":"3.4","OldVersion":"' + inputdict.CurrentVersion + '","UCFU":{"Shortcut Name":"' + inputdict.ShortcutName + '","Current Version":"' + inputdict.CurrentVersion + '","RoutineHub ID":"' + inputdict.ID + '"}}';
+    result = '{"result":"Update","errors":"none","NewVersion":"' + apioutput.Version + '","QuickUpdate Version":"4.0","OldVersion":"' + inputdict.CurrentVersion + '","UCFU":{"Shortcut Name":"' + inputdict.ShortcutName + '","Current Version":"' + inputdict.CurrentVersion + '","RoutineHub ID":"' + inputdict.ID + '"}}';
   }
 }
 
-if (!(result.includes('a')))result = '{"result":"NO INTERNET/ROUTINEHUB DOWN","UCFU":{"Current Version":"3.4","RoutineHub ID":"2647","Service":"RoutineHub","Shortcut Name":"QuickUpdate"}}';
+if (!(result.includes('a')))result = '{"result":"Updated","errors":"NO INTERNET/ROUTINEHUB DOWN","UCFU":{"Current Version":"4.0","RoutineHub ID":"2647","Service":"RoutineHub","Shortcut Name":"QuickUpdate"}}';
 
 document.body.innerHTML = '';
 document.write(result);
